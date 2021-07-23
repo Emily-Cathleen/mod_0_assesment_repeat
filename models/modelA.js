e89c820c15d1e2995f938587e78c09ca5926052b
Emily Corcoran <cathleencorcoran@gmail.com>
2021-07-23 13:24:35 -0400

    Add airport object

---
+++ models/modelA.js
@@ -0,0 +1,23 @@
+//Part III:Complete program specific challenge
+
+//Using an object, we want you to model an AIRPORT - be sure your model includes the following:
+
+//Created new variable that follows strong naming conventions and is assigned to an object
+//2 keys assigned to a string value
+//2 keys assigned to an integer or float value
+//2 keys assigned to a boolean value
+//2 keys assigned to an array value
+
+var airport = {
+  airportName: "BWI",
+  airline: "South West",
+
+  numberOfMorningFlights: 46,
+  numberOfRedEyeFlights: 12,
+
+  isFirstClass: true,
+  isBusinessClass: false,
+
+  coffeeOptions: ["Starbucks", "Dunkin Donuts"],
+  restaurantOptions: ["Obrycki's Restaurant and Bar", "The Burger Joint", "Silver Diner"]
+}
