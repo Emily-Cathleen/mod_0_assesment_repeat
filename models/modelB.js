75627a06c399906b0e1a4fbfd6b8465957754805
Emily Corcoran <cathleencorcoran@gmail.com>
2021-07-23 13:28:23 -0400

    Add vehicle object

---
+++ models/modelB.js
@@ -0,0 +1,23 @@
+//Part III: Modeling Using Objects
+
+//Using an object, we want you to model a VEHICLE - be sure your model includes the following:
+
+//Created new variable that follows strong naming conventions and is assigned to an object
+//2 keys assigned to a string value
+//2 keys assigned to an integer or float value
+//2 keys assigned to a boolean value
+//2 keys assigned to an array value
+
+var vehicle =   {
+  vehicleMake: "Hyandai",
+  vehicleModel: "Tuscon",
+
+  numberOfMiles: 60000,
+  milesPerGallon: 20,
+
+  isNew: false,
+  isUsed: true,
+
+  nickNames: ["Dazz", "Dazzler", "Cameron Diaz"],
+  package: ["Bluetooth", "Serius XM Radio", "4 Wheel Drive"]
+}
