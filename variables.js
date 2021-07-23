a858d217f75d9c4ce8e2224629ac8bf135038039
Emily Corcoran <cathleencorcoran@gmail.com>
2021-07-23 13:18:55 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,21 @@
+//Part II: Program Specific Challenge
+var famousPerson = "Obama";
+var isActive = true;
+var famousPerson = "Joe Biden";
+console.log(famousPerson);
+//rename famousPerson
+
+var num3 = 5;
+var num9 = 5;
+var sum = num3 + num9;
+console.log(sum);
+//sum num3 and num9
+
+var animals = ['cat', 'bat', 'dog'];
+animals.shift(1);
+console.log(animals);
+//remove first animal from array
+
+var famousPersonLength = famousPerson.length;
+console.log(famousPersonLength);
+//length of famousPerson strings
